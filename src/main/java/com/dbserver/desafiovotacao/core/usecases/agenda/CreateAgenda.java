3f63c2907908fe5e0268c41e211c566f1cfc37d5
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-05-10 14:00:15 -0300

    feat: Created the usecase CreateAgenda

---
+++ src/main/java/com/dbserver/desafiovotacao/core/usecases/agenda/CreateAgenda.java
@@ -0,0 +1,22 @@
+package com.dbserver.desafiovotacao.core.usecases.agenda;
+
+import com.dbserver.desafiovotacao.core.dtos.AgendaDTO;
+import com.dbserver.desafiovotacao.core.entities.AgendaBO;
+import com.dbserver.desafiovotacao.core.mappers.AgendaMapper;
+import com.dbserver.desafiovotacao.core.repository.IAgendaRepository;
+
+public class CreateAgenda {
+
+    private final IAgendaRepository repository;
+
+    public CreateAgenda(IAgendaRepository repository) {
+        this.repository = repository;
+    }
+
+    public AgendaDTO execute(AgendaDTO dto) {
+        AgendaBO entity = repository.persist(AgendaMapper.toBO(dto));
+
+        return AgendaMapper.toDTO(entity);
+    }
+
+}
