c3244c15a5b7b4fb8ae6868926ea689db42285da
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-05-10 13:59:39 -0300

    feat: Created PostgresAgenda entity

---
+++ src/main/java/com/dbserver/desafiovotacao/infra/postgres/models/PostgresAgenda.java
@@ -0,0 +1,28 @@
+package com.dbserver.desafiovotacao.infra.postgres.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "agendas")
+public class PostgresAgenda {
+
+    @Id
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+}
