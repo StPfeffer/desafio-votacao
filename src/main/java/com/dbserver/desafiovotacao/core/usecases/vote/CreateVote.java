a3b743937747279bc1098b667e02000d07680077
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-05-10 15:31:32 -0300

    feat: Created the usecase CreateVote

---
+++ src/main/java/com/dbserver/desafiovotacao/core/usecases/vote/CreateVote.java
@@ -0,0 +1,22 @@
+package com.dbserver.desafiovotacao.core.usecases.vote;
+
+import com.dbserver.desafiovotacao.core.dtos.VoteDTO;
+import com.dbserver.desafiovotacao.core.entities.VoteBO;
+import com.dbserver.desafiovotacao.core.mappers.VoteMapper;
+import com.dbserver.desafiovotacao.core.repository.IVoteRepository;
+
+public class CreateVote {
+
+    private final IVoteRepository repository;
+
+    public CreateVote(IVoteRepository repository) {
+        this.repository = repository;
+    }
+
+    public VoteDTO execute(VoteDTO dto) {
+        VoteBO entity = repository.persist(VoteMapper.toBO(dto));
+
+        return VoteMapper.toDTO(entity);
+    }
+
+}
