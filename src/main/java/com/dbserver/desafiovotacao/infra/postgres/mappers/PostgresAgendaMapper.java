2cb6aa7910ae5452b30a59e3bfabfa6fa9c450d2
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-05-10 13:51:53 -0300

    refactor: Renamed AgendaMapper to PostgresAgendaMapper

---
+++ src/main/java/com/dbserver/desafiovotacao/infra/postgres/mappers/PostgresAgendaMapper.java
@@ -0,0 +1,30 @@
+package com.dbserver.desafiovotacao.infra.postgres.mappers;
+
+import com.dbserver.desafiovotacao.core.entities.AgendaBO;
+import com.dbserver.desafiovotacao.infra.postgres.models.PostgresAgenda;
+
+import java.util.UUID;
+
+public class PostgresAgendaMapper {
+
+    public static AgendaBO toDomain(PostgresAgenda entity) {
+        AgendaBO domain = new AgendaBO();
+
+        domain.setId(entity.getId().toString());
+        domain.setTitle(entity.getTitle());
+        domain.setDescription(entity.getDescription());
+
+        return domain;
+    }
+
+    public static PostgresAgenda toEntity(AgendaBO domain) {
+        PostgresAgenda entity = new PostgresAgenda();
+
+        entity.setId(domain.getId() == null ? UUID.randomUUID() : UUID.fromString(domain.getId()));
+        entity.setTitle(domain.getTitle());
+        entity.setDescription(domain.getDescription());
+
+        return entity;
+    }
+
+}
