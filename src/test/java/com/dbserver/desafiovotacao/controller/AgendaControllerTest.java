8426d04c1773267af65abde6234e7b6d7b1dcc2b
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-05-13 15:39:55 -0300

    test: Added more tests to AgendaControllerTest

--- src/test/java/com/dbserver/desafiovotacao/controller/AgendaControllerTest.java
+++ src/test/java/com/dbserver/desafiovotacao/controller/AgendaControllerTest.java
@@ -2,7 +2,6 @@ package com.dbserver.desafiovotacao.controller;
 
 import com.dbserver.desafiovotacao.core.dtos.AgendaDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -32,7 +31,7 @@ class AgendaControllerTest {
     @Test
     void listAllAgendas() throws Exception {
         this.mockMvc.perform(get("/api/v1/agendas")
-                .contentType(MediaType.APPLICATION_JSON))
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$").isArray());
@@ -44,7 +43,7 @@ class AgendaControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$").isNotEmpty())
-                .andExpect(jsonPath("$").isString());
+                .andExpect(jsonPath("$").isMap());
     }
 
     @Test
@@ -53,16 +52,86 @@ class AgendaControllerTest {
         agenda.setDescription("Agenda description");
 
         this.mockMvc.perform(post("/api/v1/agendas")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsBytes(agenda)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(agenda)))
                 .andExpect(status().isBadRequest())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$").isNotEmpty())
                 .andExpect(jsonPath("message").value("Missing required property 'title'"));
     }
 
+    @Test
+    void openAgenda_withinInvalidAgendaId() throws Exception {
+        this.mockMvc.perform(get("/api/v1/agendas/d9b82a9b-d9f4-4b48-b60b-f3ad59729449/open")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("message").value("There is no agenda with the provided ID"));
+    }
+
+    @Test
+    void openAgenda_alreadyOpen() throws Exception {
+        this.buildResultActionsForAgenda()
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$").isMap());
+
+        this.mockMvc.perform(post("/api/v1/agendas/d9b82a9b-d9f4-4b48-b60b-f3ad59729463/open")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$").isMap());
+
+        this.mockMvc.perform(post("/api/v1/agendas/d9b82a9b-d9f4-4b48-b60b-f3ad59729463/open")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("message").value("The session of the agenda associated with the provided ID is already opened"));
+    }
+
+    @Test
+    void openAgenda() throws Exception {
+        this.buildResultActionsForAgenda()
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$").isMap());
+
+        this.mockMvc.perform(post("/api/v1/agendas/d9b82a9b-d9f4-4b48-b60b-f3ad59729463/open")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$").isMap());
+    }
+
+    @Test
+    void getResults_withinInvalidAgendaId() throws Exception {
+        this.mockMvc.perform(get("/api/v1/agendas/d9b82a9b-d9f4-4b48-b60b-f3ad59729449/result")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("message").value("There is no agenda with the provided ID"));
+    }
+
+    @Test
+    void getResults_withinSessionNotFinished() throws Exception {
+        this.buildResultActionsForAgenda()
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$").isMap());
+
+        this.mockMvc.perform(get("/api/v1/agendas/d9b82a9b-d9f4-4b48-b60b-f3ad59729463/result")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("message").value("The session of the agenda associated with the provided ID is not finished"));
+    }
+
     private ResultActions buildResultActionsForAgenda() throws Exception {
         AgendaDTO agenda = new AgendaDTO();
+        agenda.setId("d9b82a9b-d9f4-4b48-b60b-f3ad59729463");
         agenda.setTitle("Agenda title");
         agenda.setDescription("Agenda description");
 
