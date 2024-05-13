bcb1d634e98152dc5c7f3cb66b37f439c54a2b98
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-05-13 11:31:04 -0300

    docs: Added Javadocs to scheduler

--- src/main/java/com/dbserver/desafiovotacao/infra/spring/routines/CheckAgendaSession.java
+++ src/main/java/com/dbserver/desafiovotacao/infra/spring/routines/CheckAgendaSession.java
@@ -9,11 +9,20 @@ public class CheckAgendaSession {
 
     private final AgendaSessionService sessionService;
 
+    /**
+     * Constructs a new CheckAgendaSession instance with the provided {@link AgendaSessionService} dependency.
+     *
+     * @param sessionService The {@link AgendaSessionService} used to interact with agenda sessions.
+     */
     public CheckAgendaSession(AgendaSessionService sessionService) {
         this.sessionService = sessionService;
     }
 
-    // Each 10 seconds
+    /**
+     * Scheduled task to close agenda sessions at regular intervals.
+     * This method is invoked periodically based on the fixed delay specified.
+     * It delegates the session closing operation to the {@link AgendaSessionService}.
+     */
     @Scheduled(fixedDelay = 10000)
     public void closeSessions() {
         this.sessionService.closeSessions();
