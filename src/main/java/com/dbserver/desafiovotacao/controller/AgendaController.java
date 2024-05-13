ab2e48feba76843edd653186a5375d4020a2326b
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-05-13 11:07:01 -0300

    feat: Routes to list all Agendas, get the results and open an agenda

--- src/main/java/com/dbserver/desafiovotacao/controller/AgendaController.java
+++ src/main/java/com/dbserver/desafiovotacao/controller/AgendaController.java
@@ -1,14 +1,17 @@
 package com.dbserver.desafiovotacao.controller;
 
 import com.dbserver.desafiovotacao.core.dtos.AgendaDTO;
+import com.dbserver.desafiovotacao.core.dtos.AgendaOpenSessionRequestDTO;
+import com.dbserver.desafiovotacao.core.dtos.AgendaOpenSessionResponseDTO;
+import com.dbserver.desafiovotacao.core.dtos.AgendaResultDTO;
 import com.dbserver.desafiovotacao.service.AgendaService;
+import com.dbserver.desafiovotacao.service.AgendaSessionService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api")
@@ -16,8 +19,25 @@ public class AgendaController {
 
     private final AgendaService service;
 
-    public AgendaController(AgendaService service) {
+    private final AgendaSessionService sessionService;
+
+    public AgendaController(AgendaService service, AgendaSessionService sessionService) {
         this.service = service;
+        this.sessionService = sessionService;
+    }
+
+    @GetMapping( "/v1/agendas")
+    public ResponseEntity<List<AgendaDTO>> list() {
+        List<AgendaDTO> agendas = this.service.list();
+
+        return new ResponseEntity<>(agendas, HttpStatus.OK);
+    }
+
+    @GetMapping("/v1/agendas/{agendaId}/result")
+    public ResponseEntity<AgendaResultDTO> getResult(@PathVariable String agendaId) {
+        AgendaResultDTO agendaResult = this.service.countVotes(agendaId);
+
+        return new ResponseEntity<>(agendaResult, HttpStatus.OK);
     }
 
     @PostMapping("/v1/agendas")
@@ -25,6 +45,13 @@ public class AgendaController {
         AgendaDTO createdAgenda = this.service.create(dto);
 
         return new ResponseEntity<>(createdAgenda, HttpStatus.OK);
+    }
+
+    @PostMapping("/v1/agendas/session/open")
+    public ResponseEntity<AgendaOpenSessionResponseDTO> open(@RequestBody AgendaOpenSessionRequestDTO dto) {
+        AgendaOpenSessionResponseDTO agendaResult = this.sessionService.create(dto);
+
+        return new ResponseEntity<>(agendaResult, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/dbserver/desafiovotacao/service/AgendaService.java
+++ src/main/java/com/dbserver/desafiovotacao/service/AgendaService.java
@@ -1,9 +1,17 @@
 package com.dbserver.desafiovotacao.service;
 
 import com.dbserver.desafiovotacao.core.dtos.AgendaDTO;
+import com.dbserver.desafiovotacao.core.dtos.AgendaResultDTO;
+import com.dbserver.desafiovotacao.core.enums.EnumAgendaSessionStatus;
+import com.dbserver.desafiovotacao.core.exceptions.AgendaNotFoundException;
+import com.dbserver.desafiovotacao.core.exceptions.AgendaSessionNotFinishedException;
+import com.dbserver.desafiovotacao.core.usecases.agenda.CountAgendaVotes;
 import com.dbserver.desafiovotacao.core.usecases.agenda.CreateAgenda;
+import com.dbserver.desafiovotacao.core.usecases.agenda.ListAgendas;
 import com.dbserver.desafiovotacao.infra.postgres.repositories.PostgresAgendaRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AgendaService {
@@ -15,9 +23,36 @@ public class AgendaService {
     }
 
     public AgendaDTO create(AgendaDTO dto) {
+        if (dto.getStatus() == null) {
+            dto.setStatus(EnumAgendaSessionStatus.CREATED);
+        }
+
         CreateAgenda createAgenda = new CreateAgenda(repository);
 
         return createAgenda.execute(dto);
+    }
+
+    public List<AgendaDTO> list() {
+        ListAgendas listAgendas = new ListAgendas(repository);
+
+        return listAgendas.execute();
+    }
+
+    public AgendaResultDTO countVotes(String agendaId) {
+        if (!this.isSessionFinished(agendaId)) {
+            throw new AgendaSessionNotFinishedException();
+        }
+
+        CountAgendaVotes countAgendaVotes = new CountAgendaVotes(repository);
+
+        return countAgendaVotes.execute(agendaId);
+    }
+
+    private boolean isSessionFinished(String agendaId) {
+        AgendaDTO agenda = this.repository.findById(agendaId)
+                .orElseThrow(AgendaNotFoundException::new);
+
+        return agenda.getStatus() == EnumAgendaSessionStatus.CLOSED;
     }
 
 }
