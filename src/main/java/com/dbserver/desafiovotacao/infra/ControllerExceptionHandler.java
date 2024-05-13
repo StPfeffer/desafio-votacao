d85f0bb4750d0b56a17893157944904fa6a4cc95
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-05-13 11:30:59 -0300

    docs: Added Javadocs to exception handler

--- src/main/java/com/dbserver/desafiovotacao/infra/ControllerExceptionHandler.java
+++ src/main/java/com/dbserver/desafiovotacao/infra/ControllerExceptionHandler.java
@@ -10,6 +10,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
+    /**
+     * Handles general exceptions that are not specifically caught by other handlers.
+     *
+     * @param exception The exception to be handled.
+     * @return {@link ResponseEntity} containing the appropriate error response.
+     */
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionDTO> threatGeneralExceptions(Exception exception) {
         ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
@@ -17,6 +23,12 @@ public class ControllerExceptionHandler {
         return ResponseEntity.internalServerError().body(exceptionDTO);
     }
 
+    /**
+     * Handles {@link DataIntegrityViolationException} which typically occurs due to data integrity constraints.
+     *
+     * @param exception The exception to be handled.
+     * @return {@link ResponseEntity} containing the bad request response.
+     */
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<ExceptionDTO> threatDataIntegrityViolationExceptions(DataIntegrityViolationException exception) {
         ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "400");
@@ -24,6 +36,12 @@ public class ControllerExceptionHandler {
         return ResponseEntity.badRequest().body(exceptionDTO);
     }
 
+    /**
+     * Handles {@link AbstractException}, which is a custom exception type for the application.
+     *
+     * @param exception The exception to be handled.
+     * @return {@link ResponseEntity} containing the appropriate error response based on the exception's status code.
+     */
     @ExceptionHandler(AbstractException.class)
     public ResponseEntity<ExceptionDTO> threatAgendaNotFoundException(AbstractException exception) {
         ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), String.valueOf(exception.getStatusCode()));
